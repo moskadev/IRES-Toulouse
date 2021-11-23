d4ce645901fee9f831cc7b7eaf28e7625034fc7c
moska <19352571+moskadev@users.noreply.github.com>
2021-11-23 17:48:44 +0100

    Correction d'un bug sur le JS
    TODO : Tout rassembler dans un seul plugin

--- wordpress/wp-content/plugins/create-account/add-user.php
+++ wordpress/wp-content/plugins/create-account/add-user.php
@@ -9,24 +9,6 @@ Description: Create an account without using the WordPress panel
 Author: IUT Rodez
 Version: 1.0.0
 */
-$host = "localhost";
-$db = "wordpress";
-$user = "root";
-$pass = "root";
-$charset = "utf8mb4";
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-    echo $e->getMessage();
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
 
 /**
  * Creation of the plugin page
@@ -62,7 +44,7 @@ add_action("admin_menu", "my_page");
  *      - Add the user to the database
  */
 function page_content() {?>
-    <h1>Créer un compte d"un utilisateur.</h1>
+    <h1>Créer un compte d'un utilisateur.</h1>
 
     <form method="post" name="createuser" id="createuser" class="validate" novalidate="novalidate"
         <?php
@@ -70,14 +52,14 @@ function page_content() {?>
          * This action is documented in wp-admin/user-new.php.
          */
         do_action("user_new_form_tag"); ?>>
-    
+
         <input name="action" type="hidden" value="createuser"/>
         <?php
-    
+
         wp_nonce_field("create-user", "_wpnonce_create-user");
         // Load past data, otherwise set a default value
         $creating = isset($_POST["createuser"]);
-    
+
         $new_user_login = $creating && isset($_POST["user_login"]) ? wp_unslash($_POST["user_login"]) : "";
         $new_user_firstname = $creating && isset($_POST["first_name"]) ? wp_unslash($_POST["first_name"]) : "";
         $new_user_lastname = $creating && isset($_POST["last_name"]) ? wp_unslash($_POST["last_name"]) : "";
@@ -86,9 +68,10 @@ function page_content() {?>
         $new_user_role = $creating && isset($_POST["role"]) ? wp_unslash($_POST["role"]) : "";
         $new_user_send_notification = !($creating && !isset($_POST["send_user_notification"]));
         $new_user_ignore_pass = $creating && isset($_POST["noconfirmation"]) ? wp_unslash($_POST["noconfirmation"]) : "";
-    
+
         ?>
         <table class="form-table" role="presentation">
+
             <tr class="form-field form-required">
                 <th scope="row"><label for="email"><?php _e("Email"); ?> <span
                                 class="description"><?php _e("(required)"); ?></span></label></th>
@@ -96,24 +79,26 @@ function page_content() {?>
             </tr>
             <?php if (!is_multisite()) { ?>
                 <tr class="form-field form-required">
-                    <th scope="row"><label for="first_name"><?php _e("First Name"); ?> </label></th>
+                    <th scope="row"><label for="first_name"><?php _e("First Name"); ?> <span
+                                    class="description"><?php _e("(required)"); ?></span></label></th>
                     <td><input class="to-fill" name="first_name" type="text" id="first_name"
                                value="<?php echo esc_attr($new_user_firstname); ?>"/></td>
                 </tr>
                 <tr class="form-field form-required">
-                    <th scope="row"><label for="last_name"><?php _e("Last Name"); ?> </label></th>
+                    <th scope="row"><label for="last_name"><?php _e("Last Name"); ?> <span
+                                    class="description"><?php _e("(required)"); ?></span></label></th>
                     <td><input class="to-fill" name="last_name" type="text" id="last_name"
                                value="<?php echo esc_attr($new_user_lastname); ?>"/></td>
                 </tr>
             <?php } // End if ! is_multisite().	?>
         </table>
-    
+
         <?php
         /**
          * This action is documented in wp-admin/user-new.php
          */
         do_action("user_new_form", "add-new-user");
-    
+
         submit_button(__("Add New User"), "primary", "createuser", true, ["id" => "createusersub", "disabled" => "true"]);
         /**
          * Creation of the user's login
@@ -137,37 +122,42 @@ function page_content() {?>
         ?>
     </form>
     <script>
-        const form = document.querySelector("#createuser");
-        const formInputs = [...form.querySelectorAll(".to-fill")];
-        const buttonCreate = form.querySelector("#createusersub");
+        const forms = document.querySelectorAll("form");
+        forms.forEach(function(form) {
+            const formInputs = [...form.querySelectorAll(".to-fill")];
+            const buttonCreate = form.querySelector("input[type=submit]");
 
-        // cursor "prohibited" by default on the validation button
-        buttonCreate.style.cursor = "not-allowed";
+            // cursor "prohibited" by default on the validation button
+            buttonCreate.style.cursor = "not-allowed";
 
-        // add the input event to the form inputs
-        form.addEventListener("input", function (){
-            buttonCreate.disabled = !areFilled();
-            buttonCreate.style.cursor = areFilled() ? "pointer" : "not-allowed";
-        });
-
-        /**
-         * Verification of each input of the form by evaluating if they
-         * have a value. For e-mails, they must respect the format
-         * abc012@abc012.abc (some special characters included)
-         *
-         * @returns {this is *[]} true if the value of each input has
-         *                        been entered and follows the imposed format
-         */
-        function areFilled() {
-            return formInputs.every(input => {
-                if(input.id === "email"){
-                    // basic validation regEx for emails
-                    // https://stackoverflow.com/a/48800
-                    return input.value.match(/^\S+@\S+\.\S+$/g);
-                }
-                return input.value;
+            // add the input event to the form inputs
+            form.addEventListener("input", function () {
+                buttonCreate.disabled = !areFilled();
+                buttonCreate.style.cursor = areFilled() ? "pointer" : "not-allowed";
             });
-        }
+
+            /**
+             * Verification of each input of the form by evaluating if they
+             * have a value. For e-mails, they must respect the format
+             * abc012@abc012.abc (some special characters included)
+             *
+             * @returns {boolean} true if the value of each input has
+             *                        been entered and follows the imposed format
+             */
+            function areFilled() {
+                let filled = true;
+                formInputs.some(input => {
+                    if(filled) {
+                        // basic validation regEx for emails
+                        // https://stackoverflow.com/a/48800
+                        filled = input.id === "email" ?
+                            input.value.match(/^\S+@\S+\.\S+$/g) :
+                            input.value;
+                    }
+                });
+                return filled;
+            }
+        });
     </script>
     <?php
 }

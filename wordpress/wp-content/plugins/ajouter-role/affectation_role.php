3c8ed8d71763e815020be6f7c6daea1b16606755
MyNameisTeo <75687539+MyNameisTeo@users.noreply.github.com>
2021-12-06 12:48:26 +0100

    corrections beugs

--- wordpress/wp-content/plugins/ajouter-role/affectation_role.php
+++ wordpress/wp-content/plugins/ajouter-role/affectation_role.php
@@ -22,15 +22,15 @@ register_activation_hook( __FILE__, 'add_roles_on_plugin_activation' );
 register_deactivation_hook( __FILE__, 'delete_roles_on_plugin_deactivation' );
 
 function affectation_page() {
-    add_menu_page(
-        "Affecter un role", // Page title when the menu is selected
-        "Affecter un role", // Name of the menu
-        10, // Menu access security level
-        "affecter_role", // Menu reference name
-        "affectation_role_content", // Call the page content function
-        "dashicons-admin-users", // Menu icon
-        3 // Page position in the list
-    );
+	add_menu_page(
+		"Affecter un role", // Page title when the menu is selected
+		"Affecter un role", // Name of the menu
+		10, // Menu access security level
+		"affecter_role", // Menu reference name
+		"affectation_role_content", // Call the page content function
+		"dashicons-admin-users", // Menu icon
+		3 // Page position in the list
+	);
 }
 
 /**
@@ -44,67 +44,68 @@ add_action("admin_menu", "affectation_page");
  *      - change a permission of an user if you are admin
  */
 function affectation_role_content() {
-    $metas = [
-        "username" => "Utilisateur",
-        "membre_association" => "Membre association (autre)"
-    ];
+	$metas = [
+		"username" => "Utilisateur",
+		"membre_association" => "Membre association (autre)"
+	];
 
-    // Check that the form has been sent
-    if(isset($_POST['username']) ) {
-	    $choice = $_POST['choosen_role'];
-        $message="Le rôle a bien été modifié";
-        $type_message="error";
+	// Check that the form has been sent
+	$user_login = $_POST['username'];
+	if(isset($user_login) ) {
+		$choice = $_POST['choosen_role'];
+		$message="$user_login est maintenant $choice.";
+		$type_message="error";
 
-        // Check if the login name submit exist
-        if ( username_exists( $_POST['username'] ) != null ) {
-	        $user = get_userdatabylogin( $_POST['username'] );
+		// Check if the login name submit exist
+		if ( username_exists( $_POST['username'] ) != null ) {
+			$user = get_userdatabylogin( $_POST['username'] );
 
-            // The role for the user haven't been changed because he already had the role choose
-            if (!in_array( "$choice", (array) $user->roles )) {
-	            $user = wp_update_user( array( 'ID' => $user->id, 'role' => $choice ) );
-	            $type_message="updated";
-            } else {
+			// The role for the user haven't been changed because he already had the role choose
+			if (!in_array( "$choice", (array) $user->roles )) {
+				$user = wp_update_user( array( 'ID' => $user->id, 'role' => $choice ) );
+				$type_message="updated";
+			} else {
 
-	            // Determine the displayed role name
-	            $name_role = ( $choice === 'contributor' )
-		            ? 'membre' : ( $choice === 'author' )
-                    ? 'responsable de groupe' : 'administrateur';
-	            $message ="Rien n'a été effectué, l'utilisateur possédait déjà le rôle que vous avez affecté ($name_role)";
-            }
-	    } else if ( username_exists( $_POST['username'] ) == null ) {
-		    $message = "Rien n'a été effectué, vous n'avez pas entré d'identifiant correct";
-	    } ?>
-	    <div id="message" class="<?php echo "$type_message";?> notice is-dismissible">
+				// Determine the displayed role name
+				$name_role = ( $choice === 'contributor' )
+					? 'membre' : ( $choice === 'author' )
+						? 'responsable de groupe' : 'administrateur';
+				$message ="Rien n'a été effectué, $user_login était déjà $name_role.";
+			}
+		} else if ( username_exists( $_POST['username'] ) == null ) {
+			$message = "Rien n'a été effectué, $user_login n'est pas un identifiant valide.";
+		} ?>
+        <div id="message" class="<?php echo "$type_message";?> notice is-dismissible">
             <p><strong><?php echo "$message"; ?></strong></p>
         </div>
-    <?php
-    }
-    ?>
+		<?php
+	}
+	?>
     <form method="post" name="modifyuser" id="modifyuser" class="validate" novalidate="novalidate">
         <h1>Modifier le rôle d'un utilisateur</h1>
         <table class="form-table" role="presentation">
-            <tr class="form-field form-required form-invalid">
+            <tr class="form-field form-required">
                 <th scope="row"><label for="username"><?php _e("Username"); ?> <span
                                 class="description"><?php _e("(required)"); ?></span></label></th>
                 <td><input class="to-fill" name="username" type="text" id="username"/></td>
             </tr>
 
-        <?php if ( current_user_can('edit_users') ) { ?>
-            <tr class="form-field">
-                <th scope="row"><label for="role"><?php _e( 'Role' ); ?></label></th>
-                <td>
-                    <select name="choosen_role">
-                        <option value="contributor">Membre (contributeur)</option>
-                        <option value="author" selected>Responsable de groupe (auteur)</option>
-                        <option value="administrator">Administrateur</option>
-                    </select>
-                </td>
-            </tr>
-            <?php } ?>
-            </table>
-            <?php submit_button(__("Modifier rôle"), "primary", "modifyuser", true, ["id" => "createusersub"]);
-?>
+			<?php if ( current_user_can('edit_users') ) { ?>
+                <tr class="form-field">
+                    <th scope="row"><label for="role"><?php _e( 'Role' ); ?></label></th>
+                    <td>
+                        <select name="choosen_role">
+                            <option value="contributor">Membre (contributeur)</option>
+                            <option value="author" selected>Responsable de groupe (auteur)</option>
+                            <option value="administrator">Administrateur</option>
+                        </select>
+                    </td>
+                </tr>
+			<?php } ?>
+        </table>
+		<?php submit_button(__("Modifier rôle"), "primary", "modifyuser", true, ["id" => "createusersub"]);
+		?>
     </form>
-<?php
+	<?php
 }
-            ?>
+?>

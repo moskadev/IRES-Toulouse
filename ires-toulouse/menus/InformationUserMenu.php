7d1f10c752cd44166d5dfa87b23edbe30840a846
Yohann May <75686878+Yohann-May@users.noreply.github.com>
2021-12-23 10:43:08 +0100

    Ajout des titres et ordre des informations

--- ires-toulouse/menus/InformationUserMenu.php
+++ ires-toulouse/menus/InformationUserMenu.php
@@ -48,7 +48,7 @@ class InformationUserMenu extends IresMenu {
 		parent::__construct( "Vos informations", // Page title when the menu is selected
 			"Vos informations", // Name of the menu
 			0, // Menu access security level
-			"dashicons-id-alt", // Menu icon
+			"dashicons-admin-users", // Menu icon
 			3 // Page position in the list
 		);
 	}
@@ -57,9 +57,11 @@ class InformationUserMenu extends IresMenu {
 	 * Content of the page
 	 */
 	public function getContent() : void {
-        global $wpdb;
-		?> <h1>Vos informations :</h1><?php
-
+		/*
+		 * SQL Request
+         * TODO move to another file to respect the MVC method
+		 */
+		global $wpdb;
         $userId = get_current_user_id();
         $userDatas = $wpdb->get_results(
                 $wpdb->prepare("SELECT * FROM {$wpdb->prefix}usermeta WHERE user_id = %d", $userId),
@@ -94,59 +96,81 @@ class InformationUserMenu extends IresMenu {
             "first_name" => "text",
             "last_name" => "text"
         ];
-        // ARray with the text of each line
+        // Array with the text of each line
 		$correspondance = [
 			"first_name" => "Prénom",
 			"last_name" => "Nom",
+			"email" => "Email",
 			"telephone" => "Téléphone",
-			"si_animateur_titre_de_la_formation" => "Titre de la formation",
+			"diplomes" => "Diplomes",
+			"situation_pro" => "Situation professionnelle",
+			"type_etablissement" => "Type établissement",
+			"nom_etablissement" => "Nom établissement",
+			"ville_etablissement" => "Ville établissement",
+			"code_uai_rne" => "Code UAI/RNE de l'établissement scolaire",
+			"nom_chef_etablissement" => "Nom du chef de l'établissement",
+			"discipline_enseignee" => "Discipline enseignée",
+
+			"animateur_formation" => "Animateur formation PAF (année en cours)",
+			"si_animateur_titre_de_la_formation" => "Si animateur, titre de la formation",
 			"participation_labo_maths" => "Participation labo de mathématiques",
 			"membre_inspe" => "Membre INSPE",
 			"interventions_inspe" => "Interventions INSPE",
-			"manifestation" => "Manifestation",
-			"responsable_groupe" => "Responsable du groupe",
-			"groupe_recherche_action" => "Groupe recherche action",
-			"membre_association_autre" => "Membre association autre",
-			"membre_societe_savante" => "Membre societe savante",
-			"membre_association_prof" => "Membre association prof",
-			"nom_cii" => "Nom CII",
+
 			"membre_cii" => "Membre CII",
-			"discipline_enseignee" => "Discipline enseignée",
-			"animateur_formation" => "Animateur formation",
-			"nom_chef_etablissement" => "Nom chef établissement",
-			"code_uai_rne" => "Code UAI RNE",
-			"ville_etablissement" => "Ville établissement",
-			"nom_etablissement" => "Nom établissement",
-			"type_etablissement" => "Type établissement",
-			"situation_pro" => "Situation professionnelle",
-			"email" => "Email",
-			"diplomes" => "Diplomes",
+			"nom_cii" => "Nom de la CII",
+
+			"membre_association_prof" => "Membre association professeurs",
+			"membre_societe_savante" => "Membre société savante",
+			"membre_association_autre" => "Membre association (autre)",
+
+			"groupe_recherche_action" => "Groupe de recherche action",
+			"responsable_groupe" => "Responsable du groupe",
+			"manifestation" => "Manifestation",
 		];
+        // Array with title of each part
+        $title = [
+                0  => "Informations personnelles",
+                12 => "Formations",
+                17  => "CII IREM",
+                19  => "Associations ou sociétés savantes",
+                22  => "Activité de l'IRES de Toulouse"
+        ];
 ?>
-            <table>
+            <table class='form-table'>
 <?php
+        $count = 0;
         foreach ($correspondance as $key => $item) {
+            if (array_key_exists($count, $title)) { ?>
+                <tr class="form-field">
+                    <td colspan="2">
+                        <br><br><h2><?php echo $title[$count] ?></h2>
+                    </td>
+                </tr>
+    <?php   }
 	        foreach ($userDatas as $data) {
                 if ($data['meta_key'] === $key) {?>
-                    <tr>
-                        <td>
-                            <h3>
-                                <?php echo $item ?>
-                            </h3>
-                        </td>
-                        <td>
-                            <?php
-                            // Affichage des données
-                            if ($type[$data['meta_key']] == "boolean") { ?>
-                                <input disabled type="checkbox" <?php if ($data['meta_value'] == "true") echo "checked";?>>
-                            <?php } // endif
-                            if ($type[$data['meta_key']] == "text") { ?>
-                                <input disabled type="text" value="<?php echo $data['meta_value'] ?>">
-                            <?php } //endif ?>
-                        </td>
-                    </tr>
+                <tr class="form-field">
+                    <th>
+                        <h4>
+                            <?php echo $item ?>
+                        </h4>
+                    </th>
+                    <td>
+                        <?php
+                        // Affichage des données
+                        if ($type[$data['meta_key']] == "boolean") { ?>
+                            <input disabled type="checkbox" <?php if ($data['meta_value'] == "true") echo "checked";?>>
+                        <?php } // endif
+                        if ($type[$data['meta_key']] == "text") { ?>
+                            <input disabled type="text" value="<?php echo $data['meta_value'] ?>">
+                        <?php } //endif ?>
+                    </td>
+                </tr>
                 <?php }
             }
-        }
+            $count++;
+        }?>
+             </table> <?php
     }
 }

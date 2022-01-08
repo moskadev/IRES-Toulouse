960578c96550316924e4343b44f7180d79f54353
Yohann May <75686878+Yohann-May@users.noreply.github.com>
2022-01-08 13:21:41 +0100

    Ajout confirmation + Création groupe

--- ires-toulouse/menus/ListeGroupeMenu.php
+++ ires-toulouse/menus/ListeGroupeMenu.php
@@ -9,8 +9,8 @@ wp_enqueue_style('prefix_bootstrap');
 class ListeGroupeMenu extends IresMenu {
 
 	public function __construct() {
-		parent::__construct("Liste des groupes", // Page title when the menu is selected
-			"Liste des groupes", // Name of the menu
+		parent::__construct("Groupes", // Page title when the menu is selected
+			"Groupes", // Name of the menu
 			0, // Menu access security level
 			"dashicons-businesswoman", // Menu icon
 			3 // Page position in the list
@@ -23,6 +23,11 @@ class ListeGroupeMenu extends IresMenu {
 	 *      - create a group of user if you are admin
 	 */
 	function getContent(): void {
+        $user = wp_get_current_user();
+		$groups = self::getGroups();
+
+		//var_dump($user);
+		//var_dump($groups[0]);
 		if(isset($_POST['delete'])) {
 			$this->delete_group($_POST['delete']);
 			echo "<meta http-equiv='refresh' content='0'>";
@@ -33,11 +38,56 @@ class ListeGroupeMenu extends IresMenu {
 			$this->deleteUserGroup($str[0], $str[1]);
 		}
 
-		$groups = self::getGroups();
-		//var_dump($groups[0]); ?>
+        if (isset($_POST['addGroup']) && isset($_POST['nameAddGroup'])) {
+	        $message = "Impossible de créer le groupe.";
+            $type_message = "error";
+	        $this->create_table();
+	        if ($this->insert_data_group(esc_attr($_POST['nameAddGroup']))) {
+		        $type_message = "updated";
+		        $message = "Le groupe ".$_POST['nameAddGroup']." a était créé.";
+            }
+	        //echo "<meta http-equiv='refresh' content='0'>";
+
+            ?>
+            <!-- Affichage du message d'erreur ou de réussite en cas d'ajout d'un groupe -->
+            <div id="message" class="<?php echo "$type_message";?> notice is-dismissible">
+                <p><strong><?php echo "$message"; ?></strong></p>
+            </div>
+            <?php
+        }
+
+		?>
+
         <div>
             <h1 class="wp-heading-inline">Groupes</h1>
-            <a href="http://localhost/wordpress/wp-admin/admin.php?page=crer_un_groupe" class="page-title-action">Ajouter</a>
+
+            <?php
+            /**
+             * Ajout d'un groupe si l'utilisateur est administrateur
+             */
+            if (current_user_can('administrator')) {
+                ?>
+
+                <form action="" method="post">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-2">
+                                <label for="addGroup">Ajouter un groupe :</label>
+                            </div>
+                            <div class="col">
+                                <input type="text" class="to-fill" name="nameAddGroup" placeholder="Nom du groupe">
+                            </div>
+                            <div class="col">
+                                <input type="submit" name="addGroup" value="Ajouter" class="btn btn-outline-primary">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <?php
+            } // End if
+
+            ?>
         </div>
 
 
@@ -75,7 +125,14 @@ class ListeGroupeMenu extends IresMenu {
                     </td>
                     <td>
                         <form action="" method="post">
-                            <button type="submit" id="delete" name="delete" value="<?php echo $group['name'] ?>" class="btn btn-outline-danger btn-sm"><?php echo __('Delete') ?></button>
+                            <button type="submit"
+                                    id="delete"
+                                    name="delete"
+                                    value="<?php echo $group['name'] ?>"
+                                    class="btn btn-outline-danger btn-sm"
+                                    onclick="return confirm('Êtes vous sur de vouloir supprimer le groupe : <?php echo $group['name']; ?> ?');">
+                                    <?php echo __('Delete') ?>
+                            </button>
                         </form>
                     </td>
                     <td>
@@ -96,16 +153,23 @@ class ListeGroupeMenu extends IresMenu {
                             <tr>
                                 <td>
 			                        <?php
-			                        $data = $this->getUser($user['user_id'], "last_name");
-                                    echo $data[0]['meta_value'];
-			                        $data = $this->getUser($user['user_id'], "first_name");
-			                        echo " ".$data[0]['meta_value'];
+			                        $last_name = $this->getUser($user['user_id'], "last_name");
+                                    echo $last_name[0]['meta_value'];
+			                        $first_name = $this->getUser($user['user_id'], "first_name");
+			                        echo " ".$first_name[0]['meta_value'];
 			                        ?>
                                 </td>
                                 <td colspan="3"></td>
                                 <td>
                                     <form action="" method="post">
-                                        <button type="submit" id="deleteMember" name="deleteMember" value="<?php echo $user['user_id'].".".$group['id_group'] ?>" class="btn btn-outline-danger btn-sm"><?php echo __('Remove') ?></button>
+                                        <button type="submit"
+                                                id="deleteMember"
+                                                name="deleteMember"
+                                                value="<?php echo $user['user_id'].".".$group['id_group'] ?>"
+                                                class="btn btn-outline-danger btn-sm"
+                                                onclick="return confirm('Êtes vous sur de vouloir retirer <?php echo $first_name[0]['meta_value']." ".$last_name[0]['meta_value']; ?> du groupe <?php echo $group['name']; ?> ?');">
+                                                    <?php echo __('Remove') ?>
+                                        </button>
                                     </form>
                                 </td>
                             </tr>
@@ -209,4 +273,63 @@ class ListeGroupeMenu extends IresMenu {
         global $wpdb;
         return $wpdb->get_results($wpdb->prepare("DELETE FROM {$wpdb->prefix}groups_users WHERE user_id = %d AND group_id = %d", $userId, $groupId));
     }
+
+	/**
+	 * Looking if groups and groups_user table have been created and if they not, create then
+	 *
+	 * @param string $inputId
+	 * @return string input's value
+	 */
+	function create_table() {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'groups';
+		$sql_create_group = "CREATE TABLE $table_name (
+                id_group bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                name char(30) NOT NULL,
+                time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                creator_id bigint(20) UNSIGNED NOT NULL,
+                FOREIGN KEY (creator_id) REFERENCES wp_users(ID),
+                PRIMARY KEY  (id_group) 
+            ) $charset_collate;";
+		maybe_create_table($table_name, $sql_create_group );
+		$table_name = $wpdb->prefix . 'groups_users';
+
+		$sql_create_user_group = "CREATE TABLE $table_name (
+                user_id bigint(20) UNSIGNED NOT NULL,
+                group_id bigint(20) UNSIGNED NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES wp_users(ID),
+                FOREIGN KEY (group_id) REFERENCES wp_groups(id_group)
+            ) $charset_collate;";
+		maybe_create_table($table_name, $sql_create_user_group );
+	}
+
+	/**
+	 * Create a groups if it doesn't already exist in database
+	 *
+	 * @param $nameGroup
+	 *
+	 * @return true | false true if the group is created, else false
+	 */
+	function insert_data_group($nameGroup): bool {
+		global $wpdb;
+		$table_name = $wpdb->prefix . 'groups';
+
+		// If the name group is present 0 times in the database$
+		if ($this->groupExist($wpdb, $table_name, $nameGroup)) {
+			$creator_id = get_current_user_id();
+			$wpdb->insert(
+				$table_name,
+				array(
+					'name'=>$nameGroup,
+					'creator_id'=>$creator_id
+				),
+				array( '%s','%d')
+			);
+
+            return true;
+		}
+        return false;
+	}
 }

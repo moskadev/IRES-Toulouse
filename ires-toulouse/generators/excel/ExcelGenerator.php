368667ea90b30180fcf4eb3b787d7049022a82bd
moska <19352571+moskadev@users.noreply.github.com>
2022-03-10 12:53:21 +0100

    Simplification de la génération du XLSX

--- ires-toulouse/generators/excel/ExcelGenerator.php
+++ ires-toulouse/generators/excel/ExcelGenerator.php
@@ -6,13 +6,12 @@ use irestoulouse\generators\FileGenerator;
 
 class ExcelGenerator extends FileGenerator {
 
+    public const SHEET_NAME = "Utilisateurs";
+
     private XLSXWriter $writer;
-    private string $fullFileName;
 
     public function __construct(string $fileName) {
         parent::__construct($fileName);
-
-        $this->fullFileName = $this->fileName . ".xlsx";
         $this->writer = new XLSXWriter();
     }
 
@@ -22,13 +21,13 @@ class ExcelGenerator extends FileGenerator {
             $styles["font-style"] = "bold";
             $styles["fill"] = "#D7D7D7";
         }
-        $this->writer->writeSheetRow("Utilisateurs", $data, $styles);
+        $this->writer->writeSheetRow(self::SHEET_NAME, $data, $styles);
     }
 
     public function createBlankLines(int $quantity = 1) {
         $styles["border"] = ["left, right, top, bottom"];
         while ($quantity > 0){
-            $this->writer->writeSheetRow("Utilisateurs", [], $styles);
+            $this->writer->writeSheetRow(self::SHEET_NAME, [], $styles);
             --$quantity;
         }
     }
@@ -37,26 +36,19 @@ class ExcelGenerator extends FileGenerator {
      * @param array $users
      */
     public function generate(array $users) {
-        ob_start();
-
         header("Pragma: public");
         header("Expires: 0");
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
         header("Cache-Control: private", false);
         header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        header("Content-Disposition: attachment; filename={$this->fullFileName}");
+        header("Content-Disposition: attachment; filename={$this->fileName}.xlsx");
         header("Content-Transfer-Encoding: binary");
 
         $this->writer->setAuthor("ires-toulouse");
         $this->writer->setCompany("IRES de Toulouse");
 
         parent::generate($users);
-        $this->writer->writeToFile($this->fullFileName);
-
-        ob_clean();
-        flush();
-        readfile($this->fullFileName);
-        unlink($this->fullFileName);
+        $this->writer->writeToStdOut();
 
         exit();
     }
